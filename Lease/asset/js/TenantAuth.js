387cbb2a79d9f345ff42ea2103d7f0b7b96fe436
Jieya <Jieya@users.noreply.github.com>
2019-04-17 10:17:52 +0800

    验证码逻辑更新

--- Lease/asset/js/TenantAuth.js
+++ Lease/asset/js/TenantAuth.js
@@ -1,81 +1,174 @@
-var vm = new Vue({
+new Vue({
     el: '#app',
     data: {
-        step:1,
+        a: '1',
+        businessLicence: '',
+        certificate: '',
+        imageUrl: '',
+        step: 1,
         visible: false,
         radio: '0',
         realName: '',
         identityCode: '',
-        corporateIdCard:'',
-        corporateName:'',
-        companyName:'',
-        companyCode:'',
+        corporateIdCard: '',
+        corporateName: '',
+        companyName: '',
+        companyCode: '',
         phone: '',
         code: '',
         buttonName: "获取验证码",
         isDisabled: false,
-        fileList: [{ name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100' }, { name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100' }],
         time: 60
+    }, created: function () {
+        // `this` 指向 vm 实例
+        console.log('a is: ' + this.a)
+
+        if (localStorage.getItem('SendTime')) {
+            var vals = localStorage.getItem('SendTime');
+            var curtime = new Date().getTime();
+            
+            var sendTime =  60 -  parseInt((curtime - vals) / 1000);
+
+            console.log(curtime);
+            console.log(vals);
+            console.log(sendTime);
+            
+            if ((sendTime > 0 )) {
+                this.time = sendTime;
+                this.isDisabled = true;
+                var that = this;    //将this存为一个变量，此时的this指向obj
+                var interval = window.setInterval(function () {
+                    that.buttonName = '重新发送(' + that.time + ')';
+                    --that.time;
+                    if (that.time < 0) {
+                        that.buttonName = "重新发送";
+                        that.time = 60;
+                        that.isDisabled = false;
+                        window.clearInterval(interval);
+                    }
+                }, 1000);
+            }
+        }
+
     },
     methods: {
         sendMsg() {
-            vm.isDisabled = true;
+
+            if (this.phone.length == 0) {
+                this.$message.error('请输入手机号!');
+                return;
+            }
+
+            var curtime = new Date().getTime(); // 获取当前时间 ，转换成JSON字符串序列  
+            localStorage.setItem('SendTime', curtime);
+            console.log(curtime);
+            
+
+
+            this.isDisabled = true;
+
+
+            var that = this;    //将this存为一个变量，此时的this指向obj
+
             var interval = window.setInterval(function () {
-                vm.buttonName = '重新发送(' + vm.time + ')';
-                --vm.time;
-                if (vm.time < 0) {
-                    vm.buttonName = "重新发送";
-                    vm.time = 60;
-                    vm.isDisabled = false;
+                that.buttonName = '重新发送(' + that.time + ')';
+                --that.time;
+                if (that.time < 0) {
+                    that.buttonName = "重新发送";
+                    that.time = 60;
+                    that.isDisabled = false;
                     window.clearInterval(interval);
                 }
             }, 1000);
 
         },
-        authentication(){
+        authentication() {
 
-            if(vm.radio=='0'){
+            if (this.radio == '0') {
                 //个人用户，直接认证
-   // var params = { 
-            //     OS:1,
-            //     Version : '8.0.0',
-            //     VerifyCode : '123456',
-            //     Phone : '19926431639',
-            //     Access_Token : '291b90a6-d2a5-406e-86fe-bd20e720e7ea',
-            //     Type : 0,
-            //     RealName : '张三',
-            //     IdentityCode : '432503199010054678',
-            //     Path : 0
-            // }
+                // var params = { 
+                //     OS:1,
+                //     Version : '8.0.0',
+                //     VerifyCode : '123456',
+                //     Phone : '19926431639',
+                //     Access_Token : '291b90a6-d2a5-406e-86fe-bd20e720e7ea',
+                //     Type : 0,
+                //     RealName : '张三',
+                //     IdentityCode : '432503199010054678',
+                //     Path : 0
+                // }
 
-            // vm.sendPost('http://192.168.1.88:9110/Contract/Authentication', params, (err, result) => {
-            //     if (err) {
-            //         throw err;
-            //     } else {
-            //         var obj = JSON.parse(result);
-            //         console.log('obj', obj); 
-            //     }
-            // });
-            }else{
+                // this.sendPost('http://192.168.1.88:9110/Contract/Authentication', params, (err, result) => {
+                //     if (err) {
+                //         throw err;
+                //     } else {
+                //         var obj = JSON.parse(result);
+                //         console.log('obj', obj); 
+                //     }
+                // });
+            } else {
                 //企业用户
-                vm.step = 2;
+                this.step = 2;
             }
 
-         
+        },
+        done() {
 
+        },
+        goBack() {
+            this.step = this.step - 1;
+        },
+        next() {
+            this.step = this.step + 1;
+        },
+        getBusinessLicence(file, fileList) {
+            this.getBase64(file.raw).then(res => {
+                console.log(res)
+                this.businessLicence = res;
+            });
+        },
+        getCertificate(file, fileList) {
+            this.getBase64(file.raw).then(res => {
+                console.log(res)
+                this.certificate = res;
+            });
+        },
+        getBase64(file) {
+            return new Promise(function (resolve, reject) {
+                let reader = new FileReader();
+                let imgResult = "";
+                reader.readAsDataURL(file);
+                reader.onload = function () {
+                    imgResult = reader.result;
+                };
+                reader.onerror = function (error) {
+                    reject(error);
+                };
+                reader.onloadend = function () {
+                    resolve(imgResult);
+                };
+            });
+        },
+        handleAvatarSuccess(res, file) {
+            debugger
+            this.imageUrl = URL.createObjectURL(file.raw);
+        },
+        beforeAvatarUpload(file) {
+            const isJPG = file.type === 'image/jpeg';
+            const isPNG = file.type === 'image/png';
+            const isLt2M = file.size / 1024 / 1024 < 3;
 
-
-        },done(){
-            
-        },        
-        goBack(){
-            vm.step = vm.step-1;
-        },next(){
-             vm.step =  vm.step +1;
-        },  
+            if (!isJPG && !isPNG) {
+                this.$message.error('营业执照只能是 JPG/PNG 格式!');
+            }
+            if (!isLt2M) {
+                this.$message.error('上营业执照大小不能超过 3MB!');
+            }
+            return isJPG && isLt2M;
+        },
         sendPost: function (urlStr, data, cb) {
 
-            var dataStr = "paramsJSON=" + JSON.stringify(data); 
+            var dataStr = "paramsJSON=" + JSON.stringify(data);
 
             axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
