159eb26d41175d1dead30452388ec1832d87d4e7
Jieya <Jieya@users.noreply.github.com>
2019-04-17 16:00:27 +0800

    租客实名认证

--- Lease/asset/js/TenantAuth.js
+++ Lease/asset/js/TenantAuth.js
@@ -1,6 +1,7 @@
 new Vue({
     el: '#app',
     data: {
+        skipCode: '',
         a: '1',
         businessLicence: '',
         certificate: '',
@@ -20,20 +21,25 @@ new Vue({
         isDisabled: false,
         time: 60
     }, created: function () {
-        // `this` 指向 vm 实例
-        console.log('a is: ' + this.a)
+
+        // 获取get参数 skipCode
+
+ 
+        this.skipCode = this.getQueryVariable('code');
+
+
 
         if (localStorage.getItem('SendTime')) {
             var vals = localStorage.getItem('SendTime');
             var curtime = new Date().getTime();
-            
-            var sendTime =  60 -  parseInt((curtime - vals) / 1000);
+
+            var sendTime = 60 - parseInt((curtime - vals) / 1000);
 
             console.log(curtime);
             console.log(vals);
             console.log(sendTime);
-            
-            if ((sendTime > 0 )) {
+
+            if ((sendTime > 0)) {
                 this.time = sendTime;
                 this.isDisabled = true;
                 var that = this;    //将this存为一个变量，此时的this指向obj
@@ -51,7 +57,16 @@ new Vue({
         }
 
     },
-    methods: {
+    methods: { 
+        getQueryVariable(variable) {
+            var query = window.location.search.substring(1);
+            var vars = query.split("&");
+            for (var i = 0; i < vars.length; i++) {
+                var pair = vars[i].split("=");
+                if (pair[0] == variable) { return pair[1]; }
+            }
+            return (false);
+        },
         sendMsg() {
 
             if (this.phone.length == 0) {
@@ -62,7 +77,7 @@ new Vue({
             var curtime = new Date().getTime(); // 获取当前时间 ，转换成JSON字符串序列  
             localStorage.setItem('SendTime', curtime);
             console.log(curtime);
-            
+
 
 
             this.isDisabled = true;
@@ -80,32 +95,46 @@ new Vue({
                     window.clearInterval(interval);
                 }
             }, 1000);
-
         },
         authentication() {
 
             if (this.radio == '0') {
-                //个人用户，直接认证
-                // var params = { 
-                //     OS:1,
-                //     Version : '8.0.0',
-                //     VerifyCode : '123456',
-                //     Phone : '19926431639',
-                //     Access_Token : '291b90a6-d2a5-406e-86fe-bd20e720e7ea',
-                //     Type : 0,
-                //     RealName : '张三',
-                //     IdentityCode : '432503199010054678',
-                //     Path : 0
-                // }
 
-                // this.sendPost('http://192.168.1.88:9110/Contract/Authentication', params, (err, result) => {
-                //     if (err) {
-                //         throw err;
-                //     } else {
-                //         var obj = JSON.parse(result);
-                //         console.log('obj', obj); 
-                //     }
-                // });
+                if(this.realName.length==0){
+                    this.$message.error('请输入真实姓名!');
+                    return;
+                }else if(this.identityCode.length==0){
+                    this.$message.error('请输入身份证号码!');
+                    return;
+                }else if(this.phone.length==0){
+                    this.$message.error('请输入手机号码!');
+                    return;
+                }else if(this.code.length==0){
+                    this.$message.error('请输入验证码!');
+                    return;
+                }
+ 
+                // 个人用户，直接认证
+                var params = {
+                    SkipCode: this.skipCode,
+                    VerifyCode: this.code,
+                    Phone: this.phone,
+                    Type: 0,
+                    RealName: this.realName,
+                    IdentityCode: this.identityCode,
+                    Path: 1
+                }
+
+                this.sendPost('http://192.168.1.88:9110/Contract/Authentication', params, (err, result) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        var obj = JSON.parse(result);
+                        console.log('obj', obj);
+
+                        //  window.location.href = 'https://www.baidu.com'
+                    }
+                });
             } else {
                 //企业用户
                 this.step = 2;
